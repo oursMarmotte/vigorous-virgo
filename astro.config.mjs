ddb9253c3361c1cb835d198df49fe8d1df45d008
oursMarmotte <jeangerald.centaure@gmail.com>
2025-11-12 20:34:59 +0100

    coorection fichier astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site:'https://github.com/oursMarmotte.github.io',
-    base:'/vigorous-virgo',
+    site:'https://oursMarmotte.github.io',
+    base:'vigorous-virgo',
     trailingSlash:'always'
 });
