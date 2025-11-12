bbfc7b68dfa3e6d6730debe8017cb2f04d72aa18
oursMarmotte <jeangerald.centaure@gmail.com>
2025-11-12 20:00:37 +0100

    Correction chemin asto.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site:'https://github.com/oursMarmotte.github.io',
-    base:'vigorous-virgo',
+    base:'/vigorous-virgo',
     trailingSlash:'always'
 });
