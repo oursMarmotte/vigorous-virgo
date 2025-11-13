fdc91b11a50615417ce865ba4e14a01269657f1f
oursMarmotte <jeangerald.centaure@gmail.com>
2025-11-13 09:29:30 +0100

    test sans workflow gitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-   site: 'oursMarmotte.github.io',
-  base: '/vigorous-virgo/', 
+  
 });
