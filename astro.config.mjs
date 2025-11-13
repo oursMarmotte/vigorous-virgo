913986a5c315af7b7d40a13a1e94afe78fdd4235
oursMarmotte <jeangerald.centaure@gmail.com>
2025-11-13 09:57:13 +0100

    test avec githubaction

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  
+   site: 'https://oursmarmotte.github.io',
+   base:'vigorous-virgo'
 });
